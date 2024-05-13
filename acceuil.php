9f73af66ba1644d740040813d3f3f5da461eaaeb
Chaimaâ AFKIR <112092106+Mileva000@users.noreply.github.com>
2024-05-13 12:18:54 +0100

    commit

--- acceuil.php
+++ acceuil.php
@@ -67,8 +67,7 @@
             </div>
         </div>
     </div>
-
-    <!-- Bootstrap core JavaScript-->
+              <!-- Bootstrap core JavaScript-->
     <script src="vendor/jquery/jquery.min.js"></script>
     <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
@@ -84,6 +83,7 @@
     <!-- Page level custom scripts -->
     <script src="js/demo/chart-area-demo.js"></script>
     <script src="js/demo/chart-pie-demo.js"></script>
+  
                <?php
         require("footer.php");
 ?>
--- headerAcceuil.php
+++ headerAcceuil.php
@@ -157,3 +157,6 @@
     <!-- Page level custom scripts -->
     <script src="js/demo/chart-area-demo.js"></script>
     <script src="js/demo/chart-pie-demo.js"></script>
+    
+        </html>
+    </body>
