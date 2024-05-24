6532580217b21d395108822c231c415d40bec887
Chaimaâ AFKIR <112092106+Mileva000@users.noreply.github.com>
2024-05-24 14:16:35 +0100

    chaimaa

--- ajoutertrajet&.php
+++ ajoutertrajet&.php
@@ -1,34 +1,50 @@
-
 <?php 
 require('connection.php');
+
  require("header.php");
 
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $depart = $_POST['startAddress'];
-    $destination = $_POST['endAddress'];
+    $depart = $_POST['depart'];
+    $destination = $_POST['destination'];
     $date_depart = $_POST['date_depart'];
-    $date_arrivee = $_POST['date_arrivee'];
     $passager_id = $user_id;
     $statut = 'proposé';
 
-    $sql = "INSERT INTO trajets (depart, destination, date_depart,date_arrivee, statut, passager_id) VALUES ('$depart', '$destination', '$date_depart','$date_arrivee', '$statut', '$passager_id')";
+    $sql = "INSERT INTO trajets (depart, destination, date_depart, statut, passager_id) VALUES ('$depart', '$destination', '$date_depart', '$statut', '$passager_id')";
     if ($conn->query($sql) === TRUE) {
         echo "Trajet ajouté avec succès";
     } else {
         echo "Erreur: " . $sql . "<br>" . $conn->error;
     }
 }
-?>
+?>   
 <!DOCTYPE html>
-<html>
+<html lang="en">
+
 <head>
-    <title>Leaflet Routing Machine with Turf.js and Address Input</title>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://unpkg.com/leaflet/dist/leaflet.css" />
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+     <link rel="stylesheet" href="https://unpkg.com/leaflet/dist/leaflet.css" />
     <link rel="stylesheet" href="https://unpkg.com/leaflet-routing-machine/dist/leaflet-routing-machine.css" />
     <link rel="stylesheet" href="https://unpkg.com/leaflet-control-geocoder/dist/Control.Geocoder.css" />
-    <style>
+
+    <title>TRAJETS</title>
+
+    <!-- Custom fonts for this template-->
+    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link
+        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
+        rel="stylesheet">
+
+    <!-- Custom styles for this template-->
+    <link href="css/sb-admin-2.min.css" rel="stylesheet">
+<link rel="stylesheet" href="https://unpkg.com/leaflet/dist/leaflet.css" />
+   <style>
         #map {
             height: 80vh;
         }
@@ -38,17 +54,20 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     </style>
 </head>
 <body>
-<!-- Begin Page Content -->
+
+ 
+ <!-- Begin Page Content -->
  <div class="container-fluid">
 <!-- DataTales Example -->
 <div class="card shadow mb-4">
     <div class="card-header py-3">
     </div>
-     <div id="map"></div>
+   
     <div class="col-sm-12 col-xl-6">
                         <div class="rounded h-100 p-4">
 
                             <h4 class="mb-4">Ajouter un Trajet</h4>
+  <div id="map"></div>
                             <form action="#" id="routeForm" method="post"> 
                                 <div class="mb-3">
                                     <label for="startAddress" class="form-label">Départ du trajet</label>
@@ -78,10 +97,12 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                         </div>
         </div>
 </div>
-    <script src="https://unpkg.com/leaflet/dist/leaflet.js"></script>
+<script src="https://unpkg.com/leaflet/dist/leaflet.js"></script>
     <script src="https://unpkg.com/leaflet-routing-machine/dist/leaflet-routing-machine.js"></script>
     <script src="https://unpkg.com/@turf/turf/turf.min.js"></script>
     <script src="https://unpkg.com/leaflet-control-geocoder/dist/Control.Geocoder.js"></script>
-    <script src="js/map.js"></script>
+<script src="js/map.js"></script>
+
 </body>
+
 </html>
